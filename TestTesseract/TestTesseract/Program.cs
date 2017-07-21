54380a0310273f634d5c2ed408c09abd6abdd9da
Chenglin Feng <v-chfeng@microsoft.com>
2017-07-21 16:08:00 +0800

    convert PDF to PNG/tiff

---
+++ TestTesseract/TestTesseract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace TestTesseract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MagickReadSettings settings = new MagickReadSettings();
+            // Settings the density to 300 dpi will create an image with a better quality
+            settings.Density = new Density(300, 300);
+
+            using (MagickImageCollection images = new MagickImageCollection())
+            {
+                // Add all the pages of the pdf file to the collection
+                images.Read(".\\PDF\\input0.PDF", settings);
+
+                int page = 1;
+                foreach (MagickImage image in images)
+                {
+                    // Write page to file that contains the page number
+                    image.Write("Snakeware.Page" + page + ".png");
+                    // Writing to a specific format works the same as for a single image
+                    image.Format = MagickFormat.Ptif;
+                    image.Write("Snakeware.Page" + page + ".tif");
+                    page++;
+                }
+            }
+        }
+    }
+}
